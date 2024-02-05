36aae2710f49c875f2091dd17e3a0f7ccee847a9
Krishna Khong <krishna@khong.dev>
2024-02-05 19:29:35 +0800

    feat: update page description

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import InitialPageLoad from "./InitialPageLoad";
 import './style.css';
 import BackgroundImage from "./assets/images/background.jpg";
 
-const content = new InitialPageLoad("content", "Bougie Palace", "An Immersive and Seductive Cocktail Lounge Experience");
+const content = new InitialPageLoad("content", "Bougie Palace", "Flame Grilled Lounge Experience");
 content.setOpeningHours("Open 24/7!");
 content.setLandingBackgroundImage(BackgroundImage);
 content.render();
